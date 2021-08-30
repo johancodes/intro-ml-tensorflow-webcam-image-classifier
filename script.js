6e0776cdd9344078a835e4fc3d2baa1ab5f1fd3b
Jo Han <57487220+johancodes@users.noreply.github.com>
2021-08-30 09:11:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 /***** DERIVED FROM https://www.kirupa.com/html5/accessing_your_webcam_in_html5.htm *****/
 
 const predictBtn = document.getElementById("predictBtn");
@@ -6,18 +7,12 @@ const stopBtn = document.getElementById("stopBtn");
 
 const video = document.querySelector("#videoElement");
 
+//const spacebarBtn = document.getElementById("spacebarBtn");
 
 //Activate webcam video stream
 
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia(
-    {
-      video: true,
-      video: {
-        facingMode: { exact: "environment" } //force app to only use rear-facing cam. See https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
-      }
-    }
-    )
+  navigator.mediaDevices.getUserMedia({video: true})
   .then(
     function (stream) {
       video.srcObject = stream;
@@ -25,7 +20,7 @@ if (navigator.mediaDevices.getUserMedia) {
   )
   .catch(
     function (error) {
-      console.log("Something is wrong! No rear cam detected")
+      console.log("Something is wrong!")
     }
   )
 };
@@ -44,11 +39,19 @@ function stop(e) {
 
 //stopBtn event listener
 
-stopBtn.addEventListener('click', stop);
+stopBtn.addEventListener('click', stop)
 
 //Click  predictBtn to launch prediction
 
  predictBtn.addEventListener("click", app); //ALT: predictBtn.onclick = app
+
+ //Hit spacebar to launch prediction (spacebar = keyCode 32; see https://keycode.info/)
+ 
+ window.addEventListener("keydown", event => {
+   if (event.keyCode === 32) {
+    app(); //!!! Must use () after app unlike the predictBtn event listener -- different syntax to call the function !!!
+   }
+ });
 
 //Image classifier function using MobileNet pre-trained machine learning model
 
@@ -63,7 +66,7 @@ async function app(input) {
     const net = await mobilenet.load();
     console.log("Successfully loaded model");
 
-    //Make a prediction on our image through the model
+    //Make a prediction through the model on our image
     const result = await net.classify(imageUsed);
     console.log(result);
     console.log(result[0].className);
@@ -71,4 +74,21 @@ async function app(input) {
 
 };
 
+
+
+
+
+
+/* IF FROM https://github.com/tensorflow/tfjs-models/tree/master/mobilenet
+
+const img = document.getElementById('img');
+
+  // Load the model.
+  mobilenet.load().then(model => {
+    // Classify the image.
+    model.classify(img).then(predictions => {
+      console.log('Predictions: ');
+      console.log(predictions);
+    });
+  }); */
 
